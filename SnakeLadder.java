c291e1bfb243aaa6dd05cd747bbe7bbbf9fcb05a
Daminichandrakar <damini.2201@gmail.com>
2021-08-27 16:43:13 +0530

    Added: print position after dice roll

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,7 +4,9 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		int position = 0;
+		int diceRollNo = 0;
 		while (position != 100) {
+			diceRollNo++;
 			int dieRoll = 1 + (int) Math.floor(Math.random() * 10) % 6;
 			System.out.println("Die Rolled:" + dieRoll);
 
@@ -26,6 +28,9 @@ public class SnakeLadder {
 					position -= dieRoll;
 				}
 			}
+			System.out.println(diceRollNo);
+			System.out.println("Player Position After Dice Roll " + position);
 		}
+		System.out.println("Number Of Dice Roll To Win " + diceRollNo);
 	}
 }
