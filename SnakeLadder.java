e631209884e4367b92b0e433b1ac699305653ea0
Daminichandrakar <damini.2201@gmail.com>
2021-08-27 16:28:48 +0530

    Added: Dice Roll

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,6 +4,8 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		int position = 0;
+		int dieRoll = 1 + (int) Math.floor(Math.random() * 10) % 6;
+		System.out.println("Die Rolled:" + dieRoll);
 	}
 
 }
